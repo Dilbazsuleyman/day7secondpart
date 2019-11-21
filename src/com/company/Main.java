18a156e7a96329640a2e6fdfbb76cd0d6a4bff54
Dilbazsuleyman <56804853+Dilbazsuleyman@users.noreply.github.com>
2019-11-21 16:09:32 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int number=scan.nextInt();
+        System.out.println("your number is :" + number);
+    }
+}
